d39b3f88ce54b1c9da057fe4007a8c062c3bf038
tkachikartem <artem@phenomenon-studio.com>
2022-07-03 10:45:52 +0300

    change functions.php

--- functions.php
+++ functions.php
@@ -169,10 +169,10 @@ add_theme_support( 'custom-logo' );
 Remove jQuery Migrate Script from header and Load jQuery from Google API
 */
 function crunchify_stop_loading_wp_embed_and_jquery() {
-	if (!is_admin()) {
-		wp_deregister_script('wp-embed');
-		wp_deregister_script('jquery');  // Bonus: remove jquery too if it's not required
-	}
+  if (!is_admin()) {
+    wp_deregister_script('wp-embed');
+    wp_deregister_script('jquery');  // Bonus: remove jquery too if it's not required
+  }
 }
 add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
 
